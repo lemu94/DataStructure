4f2f73445ae4e91bade7ebc2daf362072c45b622
YOKOI KPAMISSA LEMUEL-ANGE <lyokoi@reference.ca>
2025-04-27 17:28:42 -0400

    feat: ajout classe Node et LinkeList

---
+++ linkedlist/linkedlist/class/LinkeList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist
+{
+    public class LinkeList<T>
+    {
+        private Node<T>? Head = null;
+        private int Count { get; set; } = 0;
+        public LinkeList() { 
+
+        
+        }
+
+
+    }
+}
---
+++ linkedlist/linkedlist/class/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist
+{
+    public class Node <T>
+    {
+        public T Value;
+        public Node<T>? Next;
+        public Node (T value) {
+            this.Value = value;
+            this.Next = null;
+        }
+    }
+}
